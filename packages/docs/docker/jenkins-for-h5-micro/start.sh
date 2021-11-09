57f30fdccc7c0089ac7ea0ad41eb225fe60ceaa4
Halo Xie <minghao.xie@foxmail.com>
2021-11-09 19:05:29 +0800

    update to save thte user/pwd/token

--- packages/docs/docker/jenkins-for-h5-micro/start.sh
+++ packages/docs/docker/jenkins-for-h5-micro/start.sh
@@ -1,6 +1,8 @@
 # 这个是本地启动的时候用的，在 lambda 上直接配置环境变量即可
-docker run --name h5-jenkins-20211108 \
--p 8070:8080 \
+# --rm delete earlier container, should stop first
+
+docker run --rm --name h5-jenkins-20211108 \
+-p 8080:8080 \
 -v /Users/haloxie/Documents/my-data/h5-jenkins-20211108-home:/var/jenkins_home \
 -e mcAlias='ycMinIO' \
 -e mcHost='http://abc.com' \
@@ -9,5 +11,6 @@ docker run --name h5-jenkins-20211108 \
 -e gitUser='haloxie' \
 -e gitPwd='abc' \
 -e gitHost='github.com' \
+-e npmRegistry='https://registry.npmjs.org/' \
 -d \
 h5-jenkins:20211108
