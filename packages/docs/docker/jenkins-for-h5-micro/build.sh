cec6722e106868c147a31a8d24f4d56671b5f4b5
Halo Xie <minghao.xie@foxmail.com>
2021-11-17 23:45:42 +0800

    update dockfile

--- packages/docs/docker/jenkins-for-h5-micro/build.sh
+++ packages/docs/docker/jenkins-for-h5-micro/build.sh
@@ -1,1 +1,3 @@
+# docker build --pull --rm -f "Dockerfile" -t h5-jenkins:20211108 "." <
+
 docker build -t h5-jenkins:20211108 .
--- packages/docs/docker/jenkins-for-h5-micro/run.sh
+++ packages/docs/docker/jenkins-for-h5-micro/run.sh
@@ -1,9 +1,9 @@
 # 这个是本地启动的时候用的，在 lambda 上直接配置环境变量即可
 # --rm delete earlier container, should stop first
+# -v /Users/haloxie/Documents/my-data/h5-jenkins-20211108-home:/var/jenkins_home \
 
 docker run --rm --name h5-jenkins-20211108 \
 -p 8080:8080 \
--v /Users/haloxie/Documents/my-data/h5-jenkins-20211108-home:/var/jenkins_home \
 -e mcAlias='ycMinIO' \
 -e mcHost='http://abc.com' \
 -e mcUser='abc' \
